8ab11df053e3b12eca6707c67db7ea0566c89200
Delilah Necrason <delilahnecrason@gmail.com>
2021-10-12 19:18:04 -0400

    Add feedback method to turn class

--- src/Turn.js
+++ src/Turn.js
@@ -15,11 +15,19 @@ class Turn {
   }
   
   evaluateGuess() {
-
+    if (this.guess === this.card.correctAnswer) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   giveFeedback() {
-    
+    if (this.evaluateGuess) {
+      return 'Correct!';
+    } else {
+      return 'Incorrect!';
+    }
   }
 }
 
